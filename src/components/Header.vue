6208e469b1991d41c2dc773268baa2afdb22faee
Danil <danil@azor.kg>
2022-12-19 09:45:34 +0600

    Add fetch

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <div>
+      <router-link to="/">home</router-link>
+    </div>
+    <search/>
+    <div>
+      <router-link to="/show">show</router-link>
+      <router-link to="/people">people</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+import Search from '@/components/Search.vue'
+export default {
+  components: {
+    Search
+  }
+}
+</script>
+
+<style lang="css" scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 10px;
+  border-bottom: 1px solid #595959;
+}
+header a {
+  margin: 0 10px;
+
+}
+</style>
